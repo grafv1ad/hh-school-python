d64977d8f1c51583f468373a5c84521b72111058
Vladislav Petrov <grafv1ad@yandex.com>
2024-01-16 17:55:48 +0500

    fix bug with Drink.production_date

--- hw/drink.py
+++ hw/drink.py
@@ -3,4 +3,4 @@ from helpers import string_to_date
 class Drink:
     def __init__(self, title=None, production_date=None) -> None:
         self.title = title
-        self.production_date = string_to_date()
+        self.production_date = string_to_date(production_date)
